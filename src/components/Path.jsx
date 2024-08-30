a591eb50d28d181e97464d99dc0419621e0be3d0
OMID GHARAVI <omidbros3@gmail.com>
2024-08-30 14:25:14 +0330

    test

--- src/components/Path.jsx
+++ src/components/Path.jsx
@@ -3,11 +3,11 @@ import { usePathname } from 'next/navigation';
 export default function Path({ childProp, excludePath = [] }) {
     const path = usePathname();
 
-    console.log('exclude path:', excludePath)
-    console.log('path:', path)
+    console.log(excludePath.includes(`${process.env.NEXT_PUBLIC_API_URL}${path}`))
 
-    const currentPath = `${process.env.NEXT_PUBLIC_API_URL}${path}`;
-    const shouldRenderChild = !excludePath.some(excluded => currentPath.endsWith(excluded));
-
-    return shouldRenderChild ? childProp : null;
+    return (
+        <>
+            {excludePath.includes(`${process.env.NEXT_PUBLIC_API_URL}${path}`) ? null : childProp}
+        </>
+    );
 }
