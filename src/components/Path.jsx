a3809285ad1c631cbe4f90c3b9723c3757148793
OMID GHARAVI <omidbros3@gmail.com>
2024-08-30 14:56:55 +0330

    last commit

--- src/components/Path.jsx
+++ src/components/Path.jsx
@@ -1,13 +1,13 @@
 'use client';
 import { usePathname } from 'next/navigation';
+
 export default function Path({ childProp, excludePath = [] }) {
     const path = usePathname();
+    const shouldRenderChild = !excludePath.includes(`${process.env.NEXT_PUBLIC_API_URL}${path}`);
 
-    console.log(excludePath.includes(`${process.env.NEXT_PUBLIC_API_URL}${path}`))
-
-    return (
+    return shouldRenderChild ? (
         <>
-            {excludePath.includes(`${process.env.NEXT_PUBLIC_API_URL}${path}`) ? '' : childProp}
+            {childProp}
         </>
-    );
+    ) : null;
 }
