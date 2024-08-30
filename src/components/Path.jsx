1224490cd114a4f1ee6db6de1e5f01f35728919f
OMID GHARAVI <omidbros3@gmail.com>
2024-08-30 14:13:16 +0330

    log

--- src/components/Path.jsx
+++ src/components/Path.jsx
@@ -2,6 +2,8 @@
 import { usePathname } from 'next/navigation';
 export default function Path({ childProp, excludePath = [] }) {
     const path = usePathname();
+    console.log("Exclude path:", excludePath)
+    console.log('path:', path)
 
     return (
         <>
