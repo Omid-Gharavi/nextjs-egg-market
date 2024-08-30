9538e1dfdfe556754bf0a440b9d1c808cdf50c8f
OMID GHARAVI <omidbros3@gmail.com>
2024-08-30 14:59:56 +0330

    undefined

--- src/components/Path.jsx
+++ src/components/Path.jsx
@@ -1,13 +1,13 @@
 'use client';
 import { usePathname } from 'next/navigation';
-
 export default function Path({ childProp, excludePath = [] }) {
     const path = usePathname();
-    const shouldRenderChild = !excludePath.includes(`${process.env.NEXT_PUBLIC_API_URL}${path}`);
 
-    return shouldRenderChild ? (
+    console.log(excludePath.includes(`${process.env.NEXT_PUBLIC_API_URL}${path}`))
+
+    return (
         <>
-            {childProp}
+            {excludePath.includes(`${process.env.NEXT_PUBLIC_API_URL}${path}`) ? undefined : childProp}
         </>
-    ) : null;
+    );
 }
