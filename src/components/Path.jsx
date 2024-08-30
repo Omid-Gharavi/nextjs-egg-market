6777fd234a148799728984583e5eeb10155ad79c
OMID GHARAVI <omidbros3@gmail.com>
2024-08-30 14:18:13 +0330

    fix exclude path

--- src/components/Path.jsx
+++ src/components/Path.jsx
@@ -2,12 +2,10 @@
 import { usePathname } from 'next/navigation';
 export default function Path({ childProp, excludePath = [] }) {
     const path = usePathname();
-    console.log("Exclude path:", excludePath)
-    console.log('path:', path)
 
     return (
         <>
-            {excludePath.includes(path) ? null : childProp}
+            {excludePath.includes(`${process.env.NEXT_PUBLIC_API_URL}${path}`) ? null : childProp}
         </>
     );
 }
